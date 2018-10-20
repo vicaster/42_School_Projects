a824bcbf1ccacdd203bacac9e14eea0bbb3862d3
Victor-louis Castera <vicaster@z2r3p5.le-101.fr>
2018-10-20 20:23:42 +0200

    vicaster101

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -5,22 +5,42 @@
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: vicaster <vicaster@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/10/18 13:59:54 by vicaster     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/18 18:57:30 by vicaster    ###    #+. /#+    ###.fr     */
+/*   Created: 2018/10/20 15:06:26 by vicaster     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/20 20:20:34 by vicaster    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int     get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-    char    tmp[BUFF_SIZE + 1];
+	int		res;
+	int		i;
+	char	*tmp;
+	char	buff[BUFF_SIZE + 1];
 
-    if (fd == -1 || line == NULL)
-        return (-1);
-    while (read(fd, tmp, BUFF_SIZE) != 0)
-    {
-        
-    }
+	i = 0;
+	if (!(*line = (char*)malloc(sizeof(char))))
+		return (0);
+	if (fd == 0)
+		return (0);
+	if (fd == -1 || line == NULL)
+		return (-1);
+	*line[0] = '\0';
+	while ((res = read(fd, buff, BUFF_SIZE)) != 0)
+	{
+		buff[res] = '\0';
+		tmp = *line;
+		*line = ft_strjoin(tmp, buff);
+//		while (*line[i] != '\n' || *line[i])
+//			i++;
+//		if (tmp[i] == '\n' || tmp[i] == '\0')
+//			return (1);
+	}
+	ft_putstr(*line);
+	if (line)
+		return (1);
+	else
+		return (0);
 }
