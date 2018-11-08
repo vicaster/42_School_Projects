0a2502ded0480c7b5b1374a708b0839c1b043d48
Victor-louis Castera <vicaster@z2r3p5.le-101.fr>
2018-11-08 16:59:08 +0100

    vicaster101

--- pre_Fillit/fillit.c
+++ pre_Fillit/fillit.c
@@ -6,7 +6,7 @@
 /*   By: vicaster <vicaster@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/02 15:42:25 by vicaster     #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/08 16:39:38 by vicaster    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/08 16:58:55 by vicaster    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,16 +15,10 @@
 
 void	fillit(int fd)
 {
-	int		x;
-	int		y;
 	int		i;
-	int		size;
 	char	**tab;
 
-	y = 0;
 	i = 0;
-	size = ((5 * i) * (5 * i) + 1);
 	ft_fill_list(fd, i);
 	tab = ft_init_tab();
-	
 }
